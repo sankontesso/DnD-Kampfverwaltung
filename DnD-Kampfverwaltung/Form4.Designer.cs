f7832df86cd6e38d5d10c019137c1c0cf80bbc5b
Jonas Kammering <jonaskammering@web.de>
2023-12-12 15:06:53 +0100

    Steuerelemente der Statusverwaltung

--- DnD-Kampfverwaltung/Form4.Designer.cs
+++ DnD-Kampfverwaltung/Form4.Designer.cs
@@ -29,6 +29,36 @@
         private void InitializeComponent()
         {
             comboBox1 = new ComboBox();
+            checkBox1 = new CheckBox();
+            label1 = new Label();
+            label2 = new Label();
+            checkBox2 = new CheckBox();
+            label3 = new Label();
+            checkBox3 = new CheckBox();
+            label4 = new Label();
+            checkBox4 = new CheckBox();
+            label5 = new Label();
+            checkBox5 = new CheckBox();
+            label6 = new Label();
+            checkBox6 = new CheckBox();
+            label7 = new Label();
+            checkBox7 = new CheckBox();
+            label8 = new Label();
+            checkBox8 = new CheckBox();
+            label9 = new Label();
+            checkBox9 = new CheckBox();
+            label10 = new Label();
+            checkBox10 = new CheckBox();
+            label11 = new Label();
+            checkBox11 = new CheckBox();
+            label12 = new Label();
+            checkBox12 = new CheckBox();
+            label13 = new Label();
+            checkBox13 = new CheckBox();
+            label14 = new Label();
+            checkBox14 = new CheckBox();
+            label15 = new Label();
+            checkBox15 = new CheckBox();
             SuspendLayout();
             // 
             // comboBox1
@@ -36,22 +66,353 @@
             comboBox1.FormattingEnabled = true;
             comboBox1.Location = new Point(12, 12);
             comboBox1.Name = "comboBox1";
-            comboBox1.Size = new Size(121, 23);
+            comboBox1.Size = new Size(201, 23);
             comboBox1.TabIndex = 0;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(12, 41);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(15, 14);
+            checkBox1.TabIndex = 2;
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(33, 41);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 3;
+            label1.Text = "label1";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(33, 56);
+            label2.Name = "label2";
+            label2.Size = new Size(38, 15);
+            label2.TabIndex = 5;
+            label2.Text = "label2";
+            // 
+            // checkBox2
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.Location = new Point(12, 56);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new Size(15, 14);
+            checkBox2.TabIndex = 4;
+            checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(33, 71);
+            label3.Name = "label3";
+            label3.Size = new Size(38, 15);
+            label3.TabIndex = 7;
+            label3.Text = "label3";
+            // 
+            // checkBox3
+            // 
+            checkBox3.AutoSize = true;
+            checkBox3.Location = new Point(12, 71);
+            checkBox3.Name = "checkBox3";
+            checkBox3.Size = new Size(15, 14);
+            checkBox3.TabIndex = 6;
+            checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(33, 86);
+            label4.Name = "label4";
+            label4.Size = new Size(38, 15);
+            label4.TabIndex = 9;
+            label4.Text = "label4";
+            // 
+            // checkBox4
+            // 
+            checkBox4.AutoSize = true;
+            checkBox4.Location = new Point(12, 86);
+            checkBox4.Name = "checkBox4";
+            checkBox4.Size = new Size(15, 14);
+            checkBox4.TabIndex = 8;
+            checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(33, 101);
+            label5.Name = "label5";
+            label5.Size = new Size(38, 15);
+            label5.TabIndex = 11;
+            label5.Text = "label5";
+            // 
+            // checkBox5
+            // 
+            checkBox5.AutoSize = true;
+            checkBox5.Location = new Point(12, 101);
+            checkBox5.Name = "checkBox5";
+            checkBox5.Size = new Size(15, 14);
+            checkBox5.TabIndex = 10;
+            checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(33, 116);
+            label6.Name = "label6";
+            label6.Size = new Size(38, 15);
+            label6.TabIndex = 13;
+            label6.Text = "label6";
+            // 
+            // checkBox6
+            // 
+            checkBox6.AutoSize = true;
+            checkBox6.Location = new Point(12, 116);
+            checkBox6.Name = "checkBox6";
+            checkBox6.Size = new Size(15, 14);
+            checkBox6.TabIndex = 12;
+            checkBox6.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(33, 131);
+            label7.Name = "label7";
+            label7.Size = new Size(38, 15);
+            label7.TabIndex = 15;
+            label7.Text = "label7";
+            // 
+            // checkBox7
+            // 
+            checkBox7.AutoSize = true;
+            checkBox7.Location = new Point(12, 131);
+            checkBox7.Name = "checkBox7";
+            checkBox7.Size = new Size(15, 14);
+            checkBox7.TabIndex = 14;
+            checkBox7.UseVisualStyleBackColor = true;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(33, 146);
+            label8.Name = "label8";
+            label8.Size = new Size(38, 15);
+            label8.TabIndex = 17;
+            label8.Text = "label8";
+            // 
+            // checkBox8
+            // 
+            checkBox8.AutoSize = true;
+            checkBox8.Location = new Point(12, 146);
+            checkBox8.Name = "checkBox8";
+            checkBox8.Size = new Size(15, 14);
+            checkBox8.TabIndex = 16;
+            checkBox8.UseVisualStyleBackColor = true;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(33, 161);
+            label9.Name = "label9";
+            label9.Size = new Size(38, 15);
+            label9.TabIndex = 19;
+            label9.Text = "label9";
+            // 
+            // checkBox9
+            // 
+            checkBox9.AutoSize = true;
+            checkBox9.Location = new Point(12, 161);
+            checkBox9.Name = "checkBox9";
+            checkBox9.Size = new Size(15, 14);
+            checkBox9.TabIndex = 18;
+            checkBox9.UseVisualStyleBackColor = true;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(33, 176);
+            label10.Name = "label10";
+            label10.Size = new Size(44, 15);
+            label10.TabIndex = 21;
+            label10.Text = "label10";
+            // 
+            // checkBox10
+            // 
+            checkBox10.AutoSize = true;
+            checkBox10.Location = new Point(12, 176);
+            checkBox10.Name = "checkBox10";
+            checkBox10.Size = new Size(15, 14);
+            checkBox10.TabIndex = 20;
+            checkBox10.UseVisualStyleBackColor = true;
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(33, 191);
+            label11.Name = "label11";
+            label11.Size = new Size(44, 15);
+            label11.TabIndex = 23;
+            label11.Text = "label11";
+            // 
+            // checkBox11
+            // 
+            checkBox11.AutoSize = true;
+            checkBox11.Location = new Point(12, 191);
+            checkBox11.Name = "checkBox11";
+            checkBox11.Size = new Size(15, 14);
+            checkBox11.TabIndex = 22;
+            checkBox11.UseVisualStyleBackColor = true;
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Location = new Point(33, 206);
+            label12.Name = "label12";
+            label12.Size = new Size(44, 15);
+            label12.TabIndex = 25;
+            label12.Text = "label12";
+            // 
+            // checkBox12
+            // 
+            checkBox12.AutoSize = true;
+            checkBox12.Location = new Point(12, 206);
+            checkBox12.Name = "checkBox12";
+            checkBox12.Size = new Size(15, 14);
+            checkBox12.TabIndex = 24;
+            checkBox12.UseVisualStyleBackColor = true;
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Location = new Point(33, 221);
+            label13.Name = "label13";
+            label13.Size = new Size(44, 15);
+            label13.TabIndex = 27;
+            label13.Text = "label13";
+            // 
+            // checkBox13
+            // 
+            checkBox13.AutoSize = true;
+            checkBox13.Location = new Point(12, 221);
+            checkBox13.Name = "checkBox13";
+            checkBox13.Size = new Size(15, 14);
+            checkBox13.TabIndex = 26;
+            checkBox13.UseVisualStyleBackColor = true;
+            // 
+            // label14
+            // 
+            label14.AutoSize = true;
+            label14.Location = new Point(33, 236);
+            label14.Name = "label14";
+            label14.Size = new Size(44, 15);
+            label14.TabIndex = 29;
+            label14.Text = "label14";
+            // 
+            // checkBox14
+            // 
+            checkBox14.AutoSize = true;
+            checkBox14.Location = new Point(12, 236);
+            checkBox14.Name = "checkBox14";
+            checkBox14.Size = new Size(15, 14);
+            checkBox14.TabIndex = 28;
+            checkBox14.UseVisualStyleBackColor = true;
+            // 
+            // label15
+            // 
+            label15.AutoSize = true;
+            label15.Location = new Point(33, 251);
+            label15.Name = "label15";
+            label15.Size = new Size(44, 15);
+            label15.TabIndex = 31;
+            label15.Text = "label15";
+            // 
+            // checkBox15
+            // 
+            checkBox15.AutoSize = true;
+            checkBox15.Location = new Point(12, 251);
+            checkBox15.Name = "checkBox15";
+            checkBox15.Size = new Size(15, 14);
+            checkBox15.TabIndex = 30;
+            checkBox15.UseVisualStyleBackColor = true;
             // 
             // Form4
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(226, 271);
+            Controls.Add(label15);
+            Controls.Add(checkBox15);
+            Controls.Add(label14);
+            Controls.Add(checkBox14);
+            Controls.Add(label13);
+            Controls.Add(checkBox13);
+            Controls.Add(label12);
+            Controls.Add(checkBox12);
+            Controls.Add(label11);
+            Controls.Add(checkBox11);
+            Controls.Add(label10);
+            Controls.Add(checkBox10);
+            Controls.Add(label9);
+            Controls.Add(checkBox9);
+            Controls.Add(label8);
+            Controls.Add(checkBox8);
+            Controls.Add(label7);
+            Controls.Add(checkBox7);
+            Controls.Add(label6);
+            Controls.Add(checkBox6);
+            Controls.Add(label5);
+            Controls.Add(checkBox5);
+            Controls.Add(label4);
+            Controls.Add(checkBox4);
+            Controls.Add(label3);
+            Controls.Add(checkBox3);
+            Controls.Add(label2);
+            Controls.Add(checkBox2);
+            Controls.Add(label1);
+            Controls.Add(checkBox1);
             Controls.Add(comboBox1);
             Name = "Form4";
             Text = "Form4";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private ComboBox comboBox1;
+        private CheckBox checkBox1;
+        private Label label1;
+        private Label label2;
+        private CheckBox checkBox2;
+        private Label label3;
+        private CheckBox checkBox3;
+        private Label label4;
+        private CheckBox checkBox4;
+        private Label label5;
+        private CheckBox checkBox5;
+        private Label label6;
+        private CheckBox checkBox6;
+        private Label label7;
+        private CheckBox checkBox7;
+        private Label label8;
+        private CheckBox checkBox8;
+        private Label label9;
+        private CheckBox checkBox9;
+        private Label label10;
+        private CheckBox checkBox10;
+        private Label label11;
+        private CheckBox checkBox11;
+        private Label label12;
+        private CheckBox checkBox12;
+        private Label label13;
+        private CheckBox checkBox13;
+        private Label label14;
+        private CheckBox checkBox14;
+        private Label label15;
+        private CheckBox checkBox15;
     }
 }
